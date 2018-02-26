88e244de3762b4e639df5eb07633483f06162234
Melissa Anderson <melanderson@ucdavis.edu>
2018-02-26 00:24:50 -0800

    Reuploading Studio 3
    So here are a few changes I made. I was able to animate using steps(), which was my main goal. Some of the other functions in the library are from the drag and drop functions...

--- studio3/script.js
+++ studio3/script.js
@@ -1,109 +1,15 @@
 "use strict";
 
-//Trying Snap, but file not working...
-/*
-var s = Snap("#svgout");
-var g = s.group();
-var cupcake = Snap.load("cupcakeImg.svg", function(loadedFragment) {
-    g.append(loadedFragment);
-    g.hover(hoverover, hoverout);
-    g.text(300, 100, 'hover over me');
-});
-
-var hoverover = function() {
-    g.animate({
-        transform: 's2r45,150,150'
-    }, 1000, mina.bounce)
-};
-var hoverout = function() {
-    g.animate({
-        transform: 's1r0,150,150'
-    }, 1000, mina.bounce)
-};
-*/
-
-/*trying out other ways and animations*/
-
-//First Image
-// Get the modal
-var modal = document.getElementById('myModal');
-
-// Get the image and insert it inside the modal - use its "alt" text as a caption
-var img = document.getElementById('myImg');
-var modalImg = document.getElementById("img01");
-var captionText = document.getElementById("caption");
-img.onclick = function() {
-    modal.style.display = "block";
-    modalImg.src = this.src;
-    captionText.innerHTML = this.alt;
+function allowDrop(ev) {
+    ev.preventDefault();
 }
 
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks on <span> , close the modal
-span.onclick = function() {
-    modal.style.display = "none";
-}
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
+function drag(ev) {
+    ev.dataTransfer.setData("text", ev.target.id);
 }
 
-//Second Image
-// Get the modal
-var modal = document.getElementById('myModal2');
-
-// Get the image and insert it inside the modal - use its "alt" text as a caption
-var img = document.getElementById('myImg2');
-var modalImg = document.getElementById("img02");
-var captionText = document.getElementById("caption2");
-img.onclick = function() {
-    modal.style.display = "block";
-    modalImg.src = this.src;
-    captionText.innerHTML = this.alt;
-}
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-    modal.style.display = "none";
-}
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
-}
-
-//third Image
-// Get the modal
-var modal = document.getElementById('myModal3');
-
-// Get the image and insert it inside the modal - use its "alt" text as a caption
-var img = document.getElementById('myImg3');
-var modalImg = document.getElementById("img03");
-var captionText = document.getElementById("caption3");
-img.onclick = function() {
-    modal.style.display = "block";
-    modalImg.src = this.src;
-    captionText.innerHTML = this.alt;
-}
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-    modal.style.display = "none";
-}
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
+function drop(ev) {
+    ev.preventDefault();
+    var data = ev.dataTransfer.getData("text");
+    ev.target.appendChild(document.getElementById(data));
 }
