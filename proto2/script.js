bb5e6e01cb2c7bd30e76a8191c75775cd4a94bd6
Melissa Anderson <melanderson@ucdavis.edu>
2018-03-12 23:49:09 -0700

    Proto2 Banner Change
    Changed the banner? Hope it works?

--- proto2/script.js
+++ proto2/script.js
@@ -1,6 +1,6 @@
 //Declare elements
 var img;
-var smallPoint, largePoint;
+var counter = 1;
 
 //Event Listener for banner
 //document.getElementById("burgers").addEventListener("click", myFunction);
@@ -16,21 +16,16 @@ var data = {
 ref.push(data);
 
 //Preload image
-function preload() {
+//function preload() {
     //img = loadImage("images/burger.png");
-    img = loadImage("https://scontent-sjc3-1.xx.fbcdn.net/v/t1.0-9/11846561_10207367454493454_1386268924484398062_n.jpg?oh=e958aa7543f77e97530c7f8aab02355e&oe=5B49A159");
-}
+    //img = loadImage("https://scontent-sjc3-1.xx.fbcdn.net/v/t1.0-9/11846561_10207367454493454_1386268924484398062_n.jpg?oh=e958aa7543f77e97530c7f8aab02355e&oe=5B49A159");
+//}
 
 //create banner
 function setup() {
     var myCanvas = createCanvas(650, 185); // To place my banner in the header //
     myCanvas.parent('mySketch');
-    smallPoint = 4;
-    largePoint = 40;
-    imageMode(CENTER);
-    noStroke();
-    background(255);
-    img.loadPixels();
+
 
     // Initialize Firebase
     var config = {
@@ -44,20 +39,69 @@ function setup() {
     firebase.initializeApp(config);
 }
 
-//draw for banner
-function draw() {
-    var pointillize = map(mouseX, 0, width, smallPoint, largePoint);
-    var x = floor(random(img.width));
-    var y = floor(random(img.height));
-    var pix = img.get(x, y);
-    fill(pix, 128);
-    ellipse(x, y, pointillize, pointillize);
-}
 
 //interactive part of banner
 //function mouseClicked() {
 //image(img,mouseX,mouseY,50,50)
 //}
+imgClickAndChange.onclick = function(){
+    if(counter == 0){
+        document.getElementById("imgClickAndChange").src = "images/bannerImg01.png";
+        counter++;
+    }
+    else if(counter == 1){
+        document.getElementById("imgClickAndChange").src = "images/bannerImg02.png";
+        counter++;
+    }
+    else if(counter == 2){
+        document.getElementById("imgClickAndChange").src = "images/bannerImg03.png";
+        counter++;
+    }
+    else if(counter == 3){
+        document.getElementById("imgClickAndChange").src = "images/bannerImg04.png";
+        counter++;
+    }else if(counter == 4){
+        document.getElementById("imgClickAndChange").src = "images/bannerImg05.png";
+        counter++;
+    }else if(counter == 5){
+        document.getElementById("imgClickAndChange").src = "images/bannerImg06.png";
+        counter++;
+    }else if(counter == 6){
+        document.getElementById("imgClickAndChange").src = "images/bannerImg07.png";
+        counter++;
+    }else if(counter == 7){
+        document.getElementById("imgClickAndChange").src = "images/bannerImg08.png";
+        counter++;
+    }else if(counter == 8){
+        document.getElementById("imgClickAndChange").src = "images/bannerImg09.png";
+        counter++;
+    }else if(counter == 9){
+        document.getElementById("imgClickAndChange").src = "images/bannerImg10.png";
+        counter++;
+    }else if(counter == 10){
+        document.getElementById("imgClickAndChange").src = "images/bannerImg11.png";
+        counter++;
+    }else if(counter == 11){
+        document.getElementById("imgClickAndChange").src = "images/bannerImg12.png";
+        counter++;
+    }else if(counter == 12){
+        document.getElementById("imgClickAndChange").src = "images/bannerImg13.png";
+        counter++;
+    }else if(counter == 13){
+        document.getElementById("imgClickAndChange").src = "images/bannerImg14.png";
+        counter++;
+    }else if(counter == 14){
+        document.getElementById("imgClickAndChange").src = "images/bannerImg15.png";
+        counter++;
+    }else if(counter == 15){
+        document.getElementById("imgClickAndChange").src = "images/bannerImg16.png";
+        counter++;
+    }else if(counter == 16){
+        document.getElementById("imgClickAndChange").src = "images/bannerImg17.png";
+        counter = 0;
+    }
+};
+
 
 //add ingredient
 function addIngredient() {
